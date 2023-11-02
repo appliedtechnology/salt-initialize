b97f1152c1aaeeb2e7adc75e63a274b15dd2cb4c
Marcus Rådell <marcus@radell.net>
2023-11-02 15:23:00 +0100

    fix spelling of product name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SALT Initializer",
-  description: "SALT Initializer",
+  title: "SALT Initialize",
+  description: "SALT Initialize",
 };
 
 export default function RootLayout({
