81bd70974026fafe6fc601040a80cfe343336f82
Marcus Rådell <marcus@radell.net>
2023-11-02 15:35:19 +0100

    add basic animation to first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black text-white">
-      <h1 className="text-5xl">
+      <h1 className="text-5xl animate__animated animate__zoomIn">
         <Link href="/begin">SALT Initialize</Link>
       </h1>
     </main>
