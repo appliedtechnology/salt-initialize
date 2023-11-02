a3777528658368a3194dd57488062f055b53d02d
Marcus Rådell <marcus@radell.net>
2023-11-02 15:26:52 +0100

    fix colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black text-white">
       <h1 className="text-5xl">
         <Link href="/begin">SALT Initialize</Link>
       </h1>
