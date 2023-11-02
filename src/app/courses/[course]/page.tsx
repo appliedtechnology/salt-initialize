5c48f73712425c24200e4b16912bc0ed755a0700
Marcus Rådell <marcus@radell.net>
2023-11-02 15:12:55 +0100

    add course page

---
+++ src/app/courses/[course]/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function course() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl">Lessons</h1>
+      <div className="flex flex-col">
+        <ul>
+          <li>Overview</li>
+          <li>Fundamentals</li>
+          <li>Advanced</li>
+        </ul>
+      </div>
+      <Link href="/lessons/js-ts/overview">Start First Lesson</Link>
+    </main>
+  );
+}
